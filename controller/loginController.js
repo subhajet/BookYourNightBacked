1eb2067ed0e37f95094c59e946394e07101a66f1
Subhajit <subhajitchoudhury34@gmail.com>
2024-06-11 12:28:42 +0530

    Cypto installed

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,4 +1,4 @@
-const cryptoJS = require('crypto-JS')
+const CryptoJS = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
 const User = require("../model/user.model")
@@ -8,7 +8,7 @@ const loginHandler = async (req,res) => {
         const user = await User.findOne({number: req.body.number})
         !user && res.status(401).json({message:"Invalid Mobile Number"})
 
-        const decodedPassword = cryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET_KEY).toString(cryptoJS.enc.Utf8);
+        const decodedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET_KEY).toString(cryptoJS.enc.Utf8);
         decodedPassword !== req.body.password && res.status(401).json({message: "Incorrect password"})
         const {password, ...rest} = user._doc;
         const accessToken = jwt.sign({username:user.username}, process.env.ACCESS_TOKEN )
