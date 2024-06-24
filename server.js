6f1f9419638575e39c2a2596a2075eea7ef2f522
Subhajit <subhajitchoudhury34@gmail.com>
2024-06-24 11:57:18 +0530

    fix: resolved cors issue

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 
 const hotelDataAddedToDBRouter = require("./routes/dataimport.router") 
 const categoryDataAddedToDBRouter = require("./routes/categoryimport.router") 
