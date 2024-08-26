3e62abe6cdbaee4be1b5d8b2bcc84bc7be1a61b5
Subhajit <subhajitchoudhury34@gmail.com>
2024-08-26 19:26:11 +0530

    spelling issue with loginController

--- controller/loginController.js
+++ controller/loginController.js
@@ -8,7 +8,7 @@ const loginHandler = async (req,res) => {
         const user = await User.findOne({number: req.body.number})
         !user && res.status(401).json({message:"Invalid Mobile Number"})
 
-        const decodedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET_KEY).toString(cryptoJS.enc.Utf8);
+        const decodedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET_KEY).toString(CryptoJS.enc.Utf8);
         decodedPassword !== req.body.password && res.status(401).json({message: "Incorrect password"})
         const {password, ...rest} = user._doc;
         const accessToken = jwt.sign({username:user.username}, process.env.ACCESS_TOKEN )
